60694657ac902516a940528cf8c32687b45d55d6
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-27 22:57:31 -0300

    Added constructor.

--- src/EmailApp/Email.java
+++ src/EmailApp/Email.java
@@ -9,4 +9,10 @@ public class Email {
     private int mailboxCapacity;
     private String alternateEmail;
 
+    // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+    }
 }
--- src/EmailApp/EmailApp.java
+++ src/EmailApp/EmailApp.java
@@ -2,6 +2,6 @@ package EmailApp;
 
 public class EmailApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Email em1 = new Email("John","Smith");
     }
 }
