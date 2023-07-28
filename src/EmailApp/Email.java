e506d4e0daefc28ad0163ad9b332c634deaae47f
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-27 23:13:43 -0300

    Added randomPassword method.

--- src/EmailApp/Email.java
+++ src/EmailApp/Email.java
@@ -9,6 +9,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
     // Constructor to receive the first name and last name
@@ -20,6 +21,10 @@ public class Email {
         // Call a method asking for the department - return the department
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        // Call a method that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
 
     // Ask for the department
@@ -31,5 +36,16 @@ public class Email {
         else if (depChoice == 2) { return "dev"; }
         else if (depChoice == 3) { return "acct"; }
         else { return ""; }
+    }
+
+    // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i=0; i<length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
     }
 }
